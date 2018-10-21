bcca646f2ca4f03e6a9dee1538f97670fd14bed5
田少杰 <tianshaojie@alibaba-inc.com>
2018-10-21 23:16:21 +0800

    library

---
+++ library/src/main/java/cn/skyui/library/utils/LocationUtils.java
@@ -0,0 +1,84 @@
+package cn.skyui.library.utils;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.orhanobut.logger.Logger;
+
+public final class LocationUtils {
+
+    private static LocationUtils mLocationUtils = null;
+    private AMapLocationClientOption mLocationOption;
+
+    private LocationUtils() {
+        initOption();
+    }
+
+    public static LocationUtils getInstance() {
+        if (mLocationUtils == null) {
+            synchronized (LocationUtils.class) {
+                if (mLocationUtils == null) {
+                    mLocationUtils = new LocationUtils();
+                }
+            }
+        }
+        return mLocationUtils;
+    }
+
+    public void startLocation(OnLocationChangedListener listener) {
+        mListener = listener;
+        init();
+    }
+
+    private void initOption(){
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption()
+                .setNeedAddress(true)// 设置是否返回地址信息（默认返回地址信息）
+                .setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy)// 设置定位模式为高精度模式
+//                .setInterval(Constants.upload_position_time)//设置定位间隔,单位毫秒,默认为2000ms
+                .setOnceLocation(true); // 设置为单次定位
+        mLocationOption.setOnceLocationLatest(true);//获取最近3s内精度最高的一次定位结果
+    }
+
+    private void init() {
+        AMapLocationClient locationClient = new AMapLocationClient(Utils.getApp());
+        locationClient.setLocationListener(amapLocation -> {
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //定位成功回调信息，设置相关消息
+                    amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                    if (mListener != null) {
+                        mListener.onSuccess(amapLocation);
+                        Logger.i(amapLocation.toStr());
+                    }
+
+                } else {
+                    if (mListener != null) {
+                        mListener.onFail(amapLocation.getErrorCode(), amapLocation.getErrorInfo());
+                    }
+                }
+            }
+        });
+
+        //设置定位参数
+        locationClient.setLocationOption(mLocationOption);
+        locationClient.startLocation();
+    }
+
+    OnLocationChangedListener mListener;
+
+    public interface OnLocationChangedListener {
+        /**
+         * 成功
+         */
+        void onSuccess(AMapLocation mapLocation);
+
+        /**
+         * 失败
+         *
+         * @param errCode 错误码
+         * @param errInfo 错误信息
+         */
+        void onFail(int errCode, String errInfo);
+    }
+}
---
+++ library/src/main/java/cn/skyui/library/utils/UiUtil.java
@@ -0,0 +1,109 @@
+package cn.skyui.library.utils;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class UiUtil {
+
+    public static void addClickEffect(View... views) {
+        for (final View view : views) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    PropertyValuesHolder scaleX;
+                    PropertyValuesHolder scaleY;
+                    PropertyValuesHolder alpha;
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+
+                            scaleX = PropertyValuesHolder.ofFloat("scaleX", 1f,
+                                    0.93f);
+                            scaleY = PropertyValuesHolder.ofFloat("scaleY", 1f,
+                                    0.93f);
+                            alpha = PropertyValuesHolder.ofFloat("alpha", 1f, 0.8f);
+
+                            ObjectAnimator.ofPropertyValuesHolder(view, alpha, scaleX, scaleY).setDuration(150).start();
+                            break;
+
+                        case MotionEvent.ACTION_CANCEL:
+                        case MotionEvent.ACTION_UP:
+                            scaleX = PropertyValuesHolder.ofFloat("scaleX", 0.93f,
+                                    1f);
+                            scaleY = PropertyValuesHolder.ofFloat("scaleY", 0.93f,
+                                    1f);
+                            alpha = PropertyValuesHolder.ofFloat("alpha", 0.8f, 1f);
+
+                            ObjectAnimator.ofPropertyValuesHolder(view, alpha, scaleX, scaleY).setDuration(200).start();
+                            break;
+
+                    }
+                    return false;
+                }
+            });
+        }
+    }
+
+
+    /**
+     * 将传入的View加上点击效果 home tab使用
+     *
+     * @param views
+     */
+    public static void addBottomBarClickEffect(View... views) {
+        for (final View view : views) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    PropertyValuesHolder scaleX;
+                    PropertyValuesHolder scaleY;
+                    PropertyValuesHolder alpha;
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            scaleX = PropertyValuesHolder.ofFloat("scaleX", 1f, 0.8f);
+                            scaleY = PropertyValuesHolder.ofFloat("scaleY", 1f, 0.8f);
+                            alpha = PropertyValuesHolder.ofFloat("alpha", 1f, 0.8f);
+                            ObjectAnimator.ofPropertyValuesHolder(view, alpha, scaleX, scaleY).setDuration(200).start();
+                            break;
+                        case MotionEvent.ACTION_CANCEL:
+                        case MotionEvent.ACTION_UP:
+                            ObjectAnimator animator;
+                            scaleX = PropertyValuesHolder.ofFloat("scaleX", 0.8f, 1.2f);
+                            scaleY = PropertyValuesHolder.ofFloat("scaleY", 0.8f, 1.2f);
+                            alpha = PropertyValuesHolder.ofFloat("alpha", 0.8f, 1f);
+                            animator = ObjectAnimator.ofPropertyValuesHolder(view, alpha, scaleX, scaleY);
+                            animator.setDuration(200);
+                            animator.addListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", 1.2f, 1.0f);
+                                    PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 1.2f, 1.0f);
+                                    ObjectAnimator.ofPropertyValuesHolder(view, scaleX, scaleY).setDuration(100).start();
+                                }
+                            });
+                            animator.start();
+                            break;
+                        default: break;
+                    }
+                    return false;
+                }
+            });
+        }
+    }
+
+    private static long lastClickTime = 0L;
+    //这里设置的时间间隔是800ms
+
+    public static boolean isFastClick(){
+        long time = System.currentTimeMillis();
+        long timeD = time - lastClickTime;
+        if(0 < timeD && timeD < 800){
+            return true;
+        }
+        lastClickTime = time;
+        return false;
+    }
+}
