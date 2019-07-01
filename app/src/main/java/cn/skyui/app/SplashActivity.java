db3a868754635d9dc857b0f20800c17fd09f6685
tianshaojie <tianshaojie@foundersc.com>
2019-07-01 11:00:36 +0800

    permission test

--- app/src/main/java/cn/skyui/app/SplashActivity.java
+++ app/src/main/java/cn/skyui/app/SplashActivity.java
@@ -1,10 +1,17 @@
 package cn.skyui.app;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.chenenyu.router.Router;
+import com.chenenyu.router.annotation.Route;
 import com.gyf.barlibrary.ImmersionBar;
+import com.qw.soul.permission.SoulPermission;
+import com.qw.soul.permission.bean.Permission;
+import com.qw.soul.permission.callbcak.CheckRequestPermissionListener;
 
 import cn.skyui.library.base.activity.BaseActivity;
 
@@ -12,33 +19,42 @@ import cn.skyui.library.base.activity.BaseActivity;
  * @author tianshaojie
  * @date 2018/1/15
  */
+@Route("splash")
 public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         BaseActivity.APP_STATUS = BaseActivity.APP_STATUS_NORMAL;
-//        setTheme(R.style.SplashThemeHuawei);
         super.onCreate(savedInstanceState);
         ImmersionBar.with(this).transparentBar().init();
-        getWindow().getDecorView().postDelayed(this::enter, 100);
 
+        SoulPermission.getInstance().checkAndRequestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
+                new CheckRequestPermissionListener() {
+                    @Override
+                    public void onPermissionOk(Permission permission) {
+                        enter();
+                    }
 
+                    @Override
+                    public void onPermissionDenied(Permission permission) {
+                        enter();
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     private void enter() {
         Router.build("main").go(this);
-//        if(User.getInstance().isLogin) {
-//            Router.build("main").go(this);
-//        } else {
-//            Router.build("login").go(this);
-//        }
         finish();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        getWindow().getDecorView().getHandler().removeCallbacksAndMessages(null);
         ImmersionBar.with(this).destroy();
     }
 }
